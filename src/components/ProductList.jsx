6327361960da29129a2eb92e38c5ab3a3b6214da
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-20 17:15:24 +0200

    add pagination, prev/next buttons functionality

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,7 +3,9 @@ import fetchProducts from "./utils/fetchProducts";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
-  {/**დამატებითი ინფო(ამშემთხვევაში ფეიჯები) */}
+  {
+    /**დამატებითი ინფო(ამშემთხვევაში ფეიჯები) */
+  }
   const [meta, setMeta] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const perPage = 10;
@@ -14,21 +16,61 @@ const ProductList = () => {
       setProducts(data.data || []);
       setMeta(data.meta || null);
       console.log(data.meta);
-    }; 
+    };
     getProducts();
-    {/*როცა currentPage შეიცვლება(კლიკზე), useEffect თავიდან გამოიძახებს API-s*/}
+    {
+      /*როცა currentPage შეიცვლება(კლიკზე), useEffect თავიდან გამოიძახებს API-s*/
+    }
   }, [currentPage]);
 
-  return ( 
-    <div className="product-list">      
+  return (
+    <>
+      <div className="flex flex-wrap gap-4 justify-center">
         {products.map((product) => (
-          <div key={product.id}>
-          <img src={product.cover_image} style={{ width: "200px", height: "250px" }} alt={product.name}/>
-          <h3>{product.name}</h3>
-          <p>$ {product.price}</p>
+          <div key={product.id} >
+            <img
+              src={product.cover_image}
+              style={{ width: "200px", height: "250px" }}
+              alt={product.name}
+            />
+            <h3>{product.name}</h3>
+            <p>$ {product.price}</p>
           </div>
         ))}
-    </div>
+      </div>
+      {/*pagination*/}
+      {meta && meta.last_page && (
+        <div className="flex items-center justify-center mt-4 space-x-2">
+          {/**prev gilaki */}
+          <button
+            onClick={() => setCurrentPage(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            Prev
+          </button>
+          {/**გვერდები */}
+          {Array.from({ length: meta.last_page }, (_, i) => {
+            const pageNumber = i + 1;
+            return (
+              <button
+                key={pageNumber}
+                onClick={() => setCurrentPage(pageNumber)}
+                disabled={currentPage === pageNumber}
+              >
+                {pageNumber}
+              </button>
+            );
+          })}
+          {/*next gilaki */}
+          <button
+            onClick={() => setCurrentPage(currentPage + 1)}
+            disabled={currentPage === meta.last_page}
+          >
+            Next
+          </button>
+        </div>
+      )}
+    </>
   );
 };
 
