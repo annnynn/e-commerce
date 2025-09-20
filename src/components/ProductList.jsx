d334f9bc8463f2f8e8bef798e39d2d7a7e3a88c1
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-20 14:49:22 +0200

    add comments

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,6 +3,7 @@ import fetchProducts from "./utils/fetchProducts";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
+  {/**დამატებითი ინფო(ამშემთხვევაში ფეიჯები) */}
   const [meta, setMeta] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
   const perPage = 10;
@@ -15,6 +16,7 @@ const ProductList = () => {
       console.log(data.meta);
     }; 
     getProducts();
+    {/*როცა currentPage შეიცვლება(კლიკზე), useEffect თავიდან გამოიძახებს API-s*/}
   }, [currentPage]);
 
   return ( 
