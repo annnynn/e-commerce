ac05c26fa724a0dcb40ef434367d7454e778e9c3
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-24 14:34:34 +0200

    create separate component for pagination

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import fetchProducts from "./utils/fetchProducts";
+import Pagination from "./Pagination";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -39,37 +40,7 @@ const ProductList = () => {
         ))}
       </div>
       {/*pagination*/}
-      {meta && meta.last_page && (
-        <div className="flex items-center justify-center mt-4 space-x-2">
-          {/**prev gilaki */}
-          <button
-            onClick={() => setCurrentPage(currentPage - 1)}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
-          {/**გვერდები */}
-          {Array.from({ length: meta.last_page }, (_, i) => {
-            const pageNumber = i + 1;
-            return (
-              <button
-                key={pageNumber}
-                onClick={() => setCurrentPage(pageNumber)}
-                disabled={currentPage === pageNumber}
-              >
-                {pageNumber}
-              </button>
-            );
-          })}
-          {/*next gilaki */}
-          <button
-            onClick={() => setCurrentPage(currentPage + 1)}
-            disabled={currentPage === meta.last_page}
-          >
-            Next
-          </button>
-        </div>
-      )}
+        <Pagination meta={meta} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
     </>
   );
 };
