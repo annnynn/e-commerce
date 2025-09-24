fb3a2675362fc3ed205e8a90e8d250b24e14edd5
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-24 16:13:53 +0200

    create separate component for individual product

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -3,6 +3,7 @@ import fetchProducts from "./utils/fetchProducts";
 import Pagination from "./Pagination";
 import PriceRangeFilter from "./PriceRangeFilter";
 import SortProducts from "./SortProducts";
+import ProductItem from "./ProductItem";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
@@ -74,25 +75,8 @@ const ProductList = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-4 px-20 my-6">
-        {filteredProducts.map((product) => (
-          <div>
-            <div
-              className="border p-2 rounded-md border-gray-200 flex justify-center items-center flex-col"
-              key={product.id}
-            >
-              <img
-                className="w-full object-cover"
-                src={product.cover_image}
-                style={{ width: "200px", height: "250px" }}
-                alt={product.name}
-              />
-            </div>
-            <h3>{product.name}</h3>
-            <p>$ {product.price}</p>
-          </div>
-        ))}
-      </div>
+      <ProductItem filteredProducts={filteredProducts}/>
+
       {/*pagination*/}
       <Pagination
         meta={meta}
