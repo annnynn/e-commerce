c665322c73f20e0936bdfd4ef89c8903f7d1fbe7
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-28 13:35:59 +0200

    update header component and readme file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,52 +1,48 @@
 import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-  const [token, setToken] = useState();
-  const [avatar, setAvatar] = useState();
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [avatarUrl, setAvatarUrl] = useState(localStorage.getItem("avatarUrl"));
+  const location = useLocation();
 
   useEffect(() => {
-    const userToken = localStorage.getItem("token");
-    const avatarUrl = localStorage.getItem("avatarUrl");
+    const handleStorageChange = () => {
+      setToken(localStorage.getItem("token"));
+      setAvatarUrl(localStorage.getItem("avatarUrl"));
+    };
 
-    setToken(userToken);
-    setAvatar(avatarUrl);
-  }, [token, avatar]);
-  const logout = () => {
-    if (token) {
-      localStorage.removeItem("token");
-    }
-  };
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
 
   return (
-    <div className="flex justify-between items-center p-4">
+    <div className="flex justify-between items-center p-4 border-b border-gray-200">
       <div className="flex items-center gap-2 cursor-pointer">
         <img src="/Vector.svg" alt="Logo" />
         <h1>RedSeam Clothing</h1>
       </div>
 
       <div className="flex items-center gap-4">
-        {token !== null ? (
+        {token ? (
           <>
-            <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
-              <img
-                src={avatar || "/avatar.jpg"}
-                alt="User Avatar"
-                className="w-full h-full object-cover"
-              />
-            </div>
-
             <Link to="/cart">
               <ShoppingCartIcon className="w-6 h-6 cursor-pointer text-gray-700" />
             </Link>
 
-            <button onClick={logout}>logout</button>
+            <div className="w-[40px] h-[40px] rounded-full">
+              <img
+                src={avatarUrl || "/avatar.png"}
+                alt="User Avatar"
+                className="w-full h-full object-cover cursor-pointer rounded-full"
+              />
+            </div>
           </>
         ) : (
           <div className="flex items-center gap-1 cursor-pointer">
-            <UserIcon className="w-[20px] h-[20px]" />
-
+            <UserIcon className="w-5 h-5" />
             <Link to="/login">
               <button className="cursor-pointer">Log in</button>
             </Link>
