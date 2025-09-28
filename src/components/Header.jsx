644c10b0c749434884b9b1de7c1a4fce5efd2597
ennyongit <112578466+annnynn@users.noreply.github.com>
2025-09-28 10:08:56 +0200

    update header
    show proper header for loggined users

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,40 @@
-import { UserIcon } from "@heroicons/react/24/solid";
+import { ShoppingCartIcon, UserIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const token = localStorage.getItem("token");
+  const avatarUrl = localStorage.getItem("avatarUrl");
+
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center p-4">
       <div className="flex items-center gap-2 cursor-pointer">
-        <img src="/Vector.svg" />
+        <img src="/Vector.svg" alt="Logo" />
         <h1>RedSeam Clothing</h1>
       </div>
 
-      <div className="flex items-center gap-1 cursor-pointer">
-        <UserIcon className="w-[20px] h-[20px]" />
-        <button className="cursor-pointer">Log in</button>
+      <div className="flex items-center gap-4">
+        {token ? (
+          <>
+            <div className="w-10 h-10 rounded-full overflow-hidden border border-gray-300">
+              <img
+                src={avatarUrl || "/avatar.png"}
+                alt="User Avatar"
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            <Link to="/cart">
+              <ShoppingCartIcon className="w-6 h-6 cursor-pointer text-gray-700" />
+            </Link>
+          </>
+        ) : (
+          <div className="flex items-center gap-1 cursor-pointer">
+            <UserIcon className="w-[20px] h-[20px]" />
+            <Link to="/login">
+              <button className="cursor-pointer">Log in</button>
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
